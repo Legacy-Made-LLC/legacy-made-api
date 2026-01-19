d2ddece8d80f7784f01bb91cb81a0c4f04fd1ce7
Jared Gibson <jared.gibson@live.com>
2026-01-19 09:35:50 -0700

    Add runtime migration for drizzle

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -1,15 +1,20 @@
 import { neon } from '@neondatabase/serverless';
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { migrate } from 'drizzle-orm/neon-http/migrator';
 import { ApiConfigService } from 'src/config/api-config.service';
 
 @Injectable()
-export class DbService {
+export class DbService implements OnModuleInit {
   public readonly drizzle: ReturnType<typeof drizzle>;
 
   constructor(private readonly config: ApiConfigService) {
     const dbUrl = this.config.get('DATABASE_URL');
     const sql = neon(dbUrl);
     this.drizzle = drizzle(sql);
+  }
+
+  async onModuleInit() {
+    await migrate(this.drizzle, { migrationsFolder: 'migrations' });
   }
 }
